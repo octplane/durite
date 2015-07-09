f1cec27ad116f99bc6825083c6567bbdfbad828d
Pierre Baillet <pierre@baillet.name>
2015-07-09 16:01:21 +0200

    More changes. examples for du

--- src/main.rs
+++ src/main.rs
@@ -11,82 +11,6 @@ use docopt::Docopt;
 
 use std::str::FromStr;
 
-extern crate libc;
-use libc::*;
-use std::ffi::CString;
-use std::default::Default;
-
-// #define MFSTYPENAMELEN  16  length of fs type name including null
-// #define MAXPATHLEN      1024
-// #define MNAMELEN        MAXPATHLEN
-   // struct statfs { /* when _DARWIN_FEATURE_64_BIT_INODE is defined */
-   //       uint32_t    f_bsize;        /* fundamental file system block size */
-   //       int32_t     f_iosize;       /* optimal transfer block size */
-   //       uint64_t    f_blocks;       /* total data blocks in file system */
-   //       uint64_t    f_bfree;        /* free blocks in fs */
-   //       uint64_t    f_bavail;       /* free blocks avail to non-superuser */
-   //       uint64_t    f_files;        /* total file nodes in file system */
-   //       uint64_t    f_ffree;        /* free file nodes in fs */
-   //       fsid_t      f_fsid;         /* file system id */
-   //       uid_t       f_owner;        /* user that mounted the filesystem */
-   //       uint32_t    f_type;         /* type of filesystem */
-   //       uint32_t    f_flags;        /* copy of mount exported flags */
-   //       uint32_t    f_fssubtype;    /* fs sub-type (flavor) */
-   //       char        f_fstypename[MFSTYPENAMELEN];   /* fs type name */
-   //       char        f_mntonname[MAXPATHLEN];        /* directory on which mounted */
-   //       char        f_mntfromname[MAXPATHLEN];      /* mounted filesystem */
-   //       uint32_t    f_reserved[8];  /* For future use */
-   //   };
-
-
-type Uid = uint32_t;
-
-
-#[repr(C)]
-struct StatFs {
-	f_bsize: uint32_t,
-	f_iosize: int32_t,
-	f_blocks: uint64_t,
-	f_bfree:  uint64_t,
-	f_bavail: uint64_t,
-	f_files:  uint64_t,
-	f_ffree:  uint64_t,
-	f_fsid:	  [uint32_t; 2],
-	f_owner:  Uid,
-	f_type:	  uint32_t,
-	f_flags:  uint32_t,
-	f_fstypename: 	[c_char; 16],
-	f_mntonname: 	[c_char; 1024],
-	f_mntfromname: 	[c_char; 1024],
-	f_reserved: 	[uint32_t; 8],
-}
-
-impl Default for StatFs {
-	fn default() -> StatFs {
-		StatFs{
-			f_bsize: 0,
-			f_iosize: 0,
-			f_blocks: 0,
-			f_bfree: 0,
-			f_bavail: 0,
-			f_files: 0,
-			f_ffree: 0,
-			f_fsid: [0; 2],
-			f_owner: 0,
-			f_type: 0,
-			f_flags: 0,
-			f_fstypename: [0; 16],
-			f_mntonname:  [0; 1024],
-			f_mntfromname:[0; 1024],
-			f_reserved: [0; 8]
-		}
-	}
-}
-
-extern {
-	fn statfs(path: *const c_char, stafs: *mut StatFs) -> size_t;
-	// statfs(const char *path, struct statfs *buf);
-}
 
 // Write the Docopt usage string. dfrites ?
 static USAGE: &'static str = "
@@ -111,6 +35,7 @@ fn disk_free() -> std::process::Output {
 	Command::new("df")
 		.arg("-k").output().unwrap_or_else(|e| { panic!("failed to execute process: {}", e) })
 }
+
 
 fn send_content(stream: &mut std::net::TcpStream, hostname: &str, probe_and_value: String, timestamp: i64) {
 	let content = format!("durite.{}.{} {}\n", hostname, probe_and_value, timestamp);
@@ -151,6 +76,8 @@ fn main() {
 						// du reports kbytes
                         let all: u64 = u64::from_str(values[1]).unwrap() / 1024;
 						let available: u64 = u64::from_str(values[3]).unwrap() / 1024;
+                        if values.len() > 8 {
+                        }
 						// let w = values[8].to_string();
 						// let mut st: StatFs = Default::default();
 						// let mp = CString::new(w.into_bytes()).unwrap();
